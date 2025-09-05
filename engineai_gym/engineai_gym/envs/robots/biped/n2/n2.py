5334d227c0f6190eaae9a361107892e79552e0a1
zhujl <792484931@qq.com>
2025-09-05 13:51:49 +0800

    Add n2 model and algo

---
+++ engineai_gym/engineai_gym/envs/robots/biped/n2/n2.py
@@ -0,0 +1,11 @@
+from isaacgym.torch_utils import *
+from isaacgym import gymtorch
+
+import torch
+from engineai_gym.envs import BipedRobot
+
+
+class N2(BipedRobot):
+    # def _get_contact_info(self):
+    #     pass
+    pass
